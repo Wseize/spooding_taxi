41bb6477349a7031ba4d6745dc83df07c49be8b6
Wseize <146534691+Wseize@users.noreply.github.com>
2025-09-13 18:14:33 +0100

    Update routing.py

--- taxi/routing.py
+++ taxi/routing.py
@@ -1,6 +1,17 @@
+# from django.urls import re_path
+# from . import consumers
+
+# websocket_urlpatterns = [
+#     re_path(r'ws/ride/(?P<passenger_id>\d+)/$', consumers.RideConsumer.as_asgi()),
+# ]
+
+
+# routing.py
 from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/ride/(?P<passenger_id>\d+)/$', consumers.RideConsumer.as_asgi()),
+    re_path(r'ws/notifications/$', consumers.NotificationsConsumer.as_asgi()),
+    re_path(r'ws/ride/(?P<ride_id>\d+)/$', consumers.RideConsumer.as_asgi()),
+    re_path(r'ws/driver/(?P<driver_id>\d+)/$', consumers.DriverConsumer.as_asgi()),
 ]
